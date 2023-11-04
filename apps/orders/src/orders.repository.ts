a02b6ea0df951dd4a117fca202c2839c190a9204
Williams Mata <williams.rm99@gmail.com>
2023-11-04 10:17:11 -0400

    Add OrdersRepository and OrderSchema to OrdersModule

---
+++ apps/orders/src/orders.repository.ts
@@ -0,0 +1,19 @@
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Order } from './schemas/order.schema';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+
+@Injectable()
+export class OrdersRepository extends AbstractRepository<Order> {
+  protected readonly logger = new Logger(OrdersRepository.name);
+
+  constructor(
+    // InjectModel is a decorator that injects a Mongoose model provider.
+    @InjectModel(Order.name) orderModule: Model<Order>,
+    // InjectConnection is a decorator that injects a Mongoose connection provider.
+    @InjectConnection() connection: Connection,
+  ) {
+    super(orderModule, connection);
+  }
+}
